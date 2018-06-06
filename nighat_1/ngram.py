b158b3e79373e0e927e862eb8fe4806ee00e45a9
usman <msohail@usc.edu>
2018-06-06 01:56:39 -0700

    nltk ngram partially working

---
+++ nighat_1/ngram.py
@@ -0,0 +1,38 @@
+import nltk
+from nltk.util import ngrams
+from nltk.corpus import gutenberg
+
+gut = list(gutenberg.words())
+
+bigram = {}
+trigram = {}
+fourgram = {}
+
+def extract_sentence(n, sent, dict):
+    temp = ngrams(sent, n)
+    for ngram in temp:
+        try:
+            dict[ngram] += 1
+        except KeyError:
+            dict[ngram] = 1
+
+for sent in gutenberg.sents():
+    extract_sentence(2, sent, bigram)
+    extract_sentence(3, sent, trigram)
+    extract_sentence(4, sent, fourgram)
+
+def percentage_dictionary(dict):
+    denominator = len(list(dict.keys()))
+    for key in dict.keys():
+        dict[key] = float(dict[key]) / float(denominator)
+
+percentage_dictionary(bigram)
+percentage_dictionary(trigram)
+percentage_dictionary(fourgram)
+
+for i in range(10):
+    print(trigram[list(trigram.keys())[i]])
+print(len(bigram))
+
+
+
